e45efdb0132092a0eb8daa15b3aed9d5d7d8d34f
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-14 22:47:38 -0300

    feat: fix the method update because before was checking CPF to update user and add method delete

--- src/main/java/br/com/natfrancisca/euvou/service/ClientService.java
+++ src/main/java/br/com/natfrancisca/euvou/service/ClientService.java
@@ -1,13 +1,12 @@
 package br.com.natfrancisca.euvou.service;
 
-import br.com.natfrancisca.euvou.dto.ResponseDTO;
 import br.com.natfrancisca.euvou.exception.CPFException;
 import br.com.natfrancisca.euvou.exception.CPFInvalidException;
+import br.com.natfrancisca.euvou.exception.ClientException;
 import br.com.natfrancisca.euvou.exception.EmailException;
 import br.com.natfrancisca.euvou.model.Client;
 import br.com.natfrancisca.euvou.repository.ClientRepository;
 import br.com.natfrancisca.euvou.util.CPFValidation;
-import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,10 +21,22 @@ public class ClientService {
         this.clientRepository = clientRepository;
     }
 
-    public Client create(Client client){
-        if(clientRepository.existsByCpf(client.getCpf())){
+    private void validationCPF(String cpf){
+        boolean cpfValidation = new CPFValidation().isValid(cpf);
+
+        if(!cpfValidation){
+            throw new CPFInvalidException();
+        }
+    }
+
+    private void isCPFAlreadyUsed(String cpf){
+        if(clientRepository.existsByCpf(cpf)){
             throw new CPFException();
         }
+    }
+
+    public Client create(Client client){
+        isCPFAlreadyUsed(client.getCpf());
 
         if(clientRepository.existsByEmail(client.getEmail())){
             throw new EmailException();
@@ -39,16 +50,34 @@ public class ClientService {
     }
 
     public Client getClient(String cpf){
-        boolean cpfValidation = new CPFValidation().isValid(cpf);
+        validationCPF(cpf);
+        isCPFAlreadyUsed(cpf);
+        return this.clientRepository.findByCpf(cpf);
+    }
 
-        if(!cpfValidation){
-            throw new CPFInvalidException();
+    public Client update(String cpf, Client client){
+        validationCPF(cpf);
+
+        if(!this.clientRepository.existsByCpf(cpf)){
+            throw new ClientException();
         }
 
-        if(!clientRepository.existsByCpf(cpf)){
-            throw new IllegalArgumentException("Não existe registro de cliente com ess CPF.");
+        Client clientToUpdate = this.clientRepository.findByCpf(cpf);
+
+        clientToUpdate.setEmail(client.getEmail());
+        clientToUpdate.setName(client.getName());
+        clientToUpdate.setCpf(client.getCpf());
+
+        return this.clientRepository.save(clientToUpdate);
+    }
+
+    public void delete(String cpf){
+        validationCPF(cpf);
+
+        if(!this.clientRepository.existsByCpf(cpf)){
+            throw new ClientException();
         }
 
-        return clientRepository.getByCpf(cpf);
+        this.clientRepository.deleteByCpf(cpf);
     }
 }
