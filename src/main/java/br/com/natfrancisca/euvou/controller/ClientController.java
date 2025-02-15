c7c765cbe1f2d5d0bacede00d184548e0ef58187
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-14 22:46:31 -0300

    feat: create methods update and delete

--- src/main/java/br/com/natfrancisca/euvou/controller/ClientController.java
+++ src/main/java/br/com/natfrancisca/euvou/controller/ClientController.java
@@ -1,11 +1,10 @@
 package br.com.natfrancisca.euvou.controller;
 
-import br.com.caelum.stella.validation.CPFValidator;
 import br.com.natfrancisca.euvou.dto.ClientDTO;
 import br.com.natfrancisca.euvou.dto.ResponseDTO;
 import br.com.natfrancisca.euvou.model.Client;
 import br.com.natfrancisca.euvou.service.ClientService;
-import br.com.natfrancisca.euvou.util.CPFValidation;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,19 +28,32 @@ public class ClientController {
     public ResponseEntity<ResponseDTO> create(@Valid @RequestBody ClientDTO clientDTO){
         Client client = clientDTO.toEntity();
         this.clientService.create(client);
-        return ResponseDTO.create(HttpStatus.CREATED, "Sucesso", "Cliente cadastrado com sucesso.");
+        return ResponseDTO.create(HttpStatus.CREATED, "Cliente cadastrado com sucesso.");
     }
 
     @GetMapping
     public ResponseEntity<ResponseDTO> getAll(){
         List<Client> clients = this.clientService.get();
         System.out.println(clients);
-        return ResponseDTO.create(HttpStatus.OK, "Sucesso", clients);
+        return ResponseDTO.create(HttpStatus.OK, clients);
     }
 
     @GetMapping("/{cpf}")
     public ResponseEntity<ResponseDTO> getByCPF(@Valid @PathVariable String cpf){
         Client client = clientService.getClient(cpf);
-        return ResponseDTO.create(HttpStatus.OK, "Sucesso", client);
+        return ResponseDTO.create(HttpStatus.OK, client);
+    }
+
+    @PutMapping("/{cpf}")
+    public ResponseEntity<ResponseDTO> update(@PathVariable String cpf, @Valid @RequestBody ClientDTO clientDTO){
+        Client client = this.clientService.update(cpf, clientDTO.toEntity());
+        return ResponseDTO.create(HttpStatus.OK, client);
+    }
+
+    @DeleteMapping("/{cpf}")
+    @Transactional
+    public ResponseEntity<ResponseDTO> delete(@PathVariable String cpf){
+        this.clientService.delete(cpf);
+        return ResponseDTO.create(HttpStatus.OK, "Cliente deletado com sucesso.");
     }
 }
