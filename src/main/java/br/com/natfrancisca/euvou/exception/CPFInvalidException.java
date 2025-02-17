9892f38a3953e600d0bce1315c6c07ee06658bd3
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-17 14:19:45 -0300

    feat: delete file

--- src/main/java/br/com/natfrancisca/euvou/exception/CPFInvalidException.java
+++
@@ -1,7 +0,0 @@
-package br.com.natfrancisca.euvou.exception;
-
-public class CPFInvalidException extends RuntimeException {
-    public CPFInvalidException() {
-        super("Digite um CPF válido ex.: xxx.xxx.xxx-xx");
-    }
-}
