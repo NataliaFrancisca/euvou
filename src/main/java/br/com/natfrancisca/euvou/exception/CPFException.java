e6510eea5f63e48c2dc49ed792f9ed567df50fd0
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-17 14:19:32 -0300

    feat: set parameter message

--- src/main/java/br/com/natfrancisca/euvou/exception/CPFException.java
+++ src/main/java/br/com/natfrancisca/euvou/exception/CPFException.java
@@ -1,7 +1,7 @@
 package br.com.natfrancisca.euvou.exception;
 
 public class CPFException extends IllegalArgumentException {
-    public CPFException(){
-        super("Já existe um cliente com este CPF.");
+    public CPFException(String message){
+        super(message);
     }
 }
