1eb870b5366ffd877bb9d4973133191f901f7e5e
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-19 23:09:03 -0300

    feat: create GET to get events by search or id and PUT and DELETE

--- src/main/java/br/com/natfrancisca/euvou/controller/EventController.java
+++ src/main/java/br/com/natfrancisca/euvou/controller/EventController.java
@@ -34,4 +34,29 @@ public class EventController {
         List<EventDTO> events = this.eventService.list();
         return APIResponseDTO.create(HttpStatus.OK, events);
     }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<EventDTO>> getAllByName(@RequestParam(name = "name") String name) {
+        List<EventDTO> events = eventService.getEventByName(name);
+        return ResponseEntity.ok(events);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<APIResponseDTO> getByID(@PathVariable Long id){
+        EventDTO event = this.eventService.getEvent(id);
+        return APIResponseDTO.create(HttpStatus.OK, event);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<APIResponseDTO> update(@PathVariable Long id, @RequestBody EventDTO eventDTO){
+        Event event = this.eventService.updateEvent(id, eventDTO.toEntity());
+        return APIResponseDTO.create(HttpStatus.OK, event);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<APIResponseDTO> deleteByID(@PathVariable Long id){
+        this.eventService.deleteEvent(id);
+        return APIResponseDTO.create(HttpStatus.OK, "Evento deletado com sucesso.");
+    }
+
 }
