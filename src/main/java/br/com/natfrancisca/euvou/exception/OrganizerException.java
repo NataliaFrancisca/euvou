b22d5a6ee73660793fa898c5931ca416eff8d901
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-24 20:40:21 -0300

    feat: delete file

--- src/main/java/br/com/natfrancisca/euvou/exception/OrganizerException.java
+++
@@ -1,7 +0,0 @@
-package br.com.natfrancisca.euvou.exception;
-
-public class OrganizerException extends RuntimeException {
-    public OrganizerException(String message) {
-        super(message);
-    }
-}
