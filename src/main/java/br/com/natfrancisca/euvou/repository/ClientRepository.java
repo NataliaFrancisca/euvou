3210b00bb35d833d5468a4fe0ec9c9344b7a6b88
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-23 23:21:25 -0300

    feat: set findByCPF to return an Optional<Client>

--- src/main/java/br/com/natfrancisca/euvou/repository/ClientRepository.java
+++ src/main/java/br/com/natfrancisca/euvou/repository/ClientRepository.java
@@ -3,9 +3,12 @@ package br.com.natfrancisca.euvou.repository;
 import br.com.natfrancisca.euvou.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
     boolean existsByCpf(String cpf);
     boolean existsByEmail(String email);
-    Client findByCpf(String cpf);
+    Optional<Client> findByCpf(String cpf);
     void deleteByCpf(String cpf);
 }
