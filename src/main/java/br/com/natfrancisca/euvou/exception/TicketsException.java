88664cf98cd1fdad446b981fece3e301978576d1
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-23 22:17:34 -0300

    feat: create TicketsException

---
+++ src/main/java/br/com/natfrancisca/euvou/exception/TicketsException.java
@@ -0,0 +1,7 @@
+package br.com.natfrancisca.euvou.exception;
+
+public class TicketsException extends IllegalArgumentException{
+    public TicketsException(String message){
+        super(message);
+    }
+}
