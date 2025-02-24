e228ad6007648f29b473ec6cfefda4fb5b0c11aa
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-23 23:40:55 -0300

    style: remove empty spaces

--- src/main/java/br/com/natfrancisca/euvou/repository/ClientRepository.java
+++ src/main/java/br/com/natfrancisca/euvou/repository/ClientRepository.java
@@ -1,10 +1,7 @@
 package br.com.natfrancisca.euvou.repository;
-
 import br.com.natfrancisca.euvou.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
-
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
     boolean existsByCpf(String cpf);
