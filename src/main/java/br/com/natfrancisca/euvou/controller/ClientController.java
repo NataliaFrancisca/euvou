63b4789cf8d2dcb478b96451d87516458b77145f
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-23 23:41:27 -0300

    refactor: remove the variables and return direct on APIResponseDTO

--- src/main/java/br/com/natfrancisca/euvou/controller/ClientController.java
+++ src/main/java/br/com/natfrancisca/euvou/controller/ClientController.java
@@ -2,7 +2,6 @@ package br.com.natfrancisca.euvou.controller;
 
 import br.com.natfrancisca.euvou.dto.ClientDTO;
 import br.com.natfrancisca.euvou.dto.APIResponseDTO;
-import br.com.natfrancisca.euvou.model.Client;
 import br.com.natfrancisca.euvou.service.ClientService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -10,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/client")
@@ -26,34 +23,29 @@ public class ClientController {
 
     @PostMapping
     public ResponseEntity<APIResponseDTO> create(@Valid @RequestBody ClientDTO clientDTO){
-        Client client = clientDTO.toEntity();
-        this.clientService.create(client);
+        this.clientService.create(clientDTO.toEntity());
         return APIResponseDTO.create(HttpStatus.CREATED, "Cliente cadastrado com sucesso.");
     }
 
     @GetMapping
     public ResponseEntity<APIResponseDTO> getAll(){
-        List<Client> clients = this.clientService.get();
-        System.out.println(clients);
-        return APIResponseDTO.create(HttpStatus.OK, clients);
+        return APIResponseDTO.create(HttpStatus.OK, this.clientService.get());
     }
 
     @GetMapping("/{cpf}")
     public ResponseEntity<APIResponseDTO> getByCPF(@Valid @PathVariable String cpf){
-        Client client = clientService.getClient(cpf);
-        return APIResponseDTO.create(HttpStatus.OK, client);
+        return APIResponseDTO.create(HttpStatus.OK, clientService.getByCPF(cpf));
     }
 
     @PutMapping("/{cpf}")
     public ResponseEntity<APIResponseDTO> update(@PathVariable String cpf, @Valid @RequestBody ClientDTO clientDTO){
-        Client client = this.clientService.update(cpf, clientDTO.toEntity());
-        return APIResponseDTO.create(HttpStatus.OK, client);
+        return APIResponseDTO.create(HttpStatus.OK, this.clientService.update(cpf, clientDTO.toEntity()));
     }
 
     @DeleteMapping("/{cpf}")
     @Transactional
     public ResponseEntity<APIResponseDTO> delete(@PathVariable String cpf){
         this.clientService.delete(cpf);
-        return APIResponseDTO.create(HttpStatus.OK, "Cliente deletado com sucesso.");
+        return APIResponseDTO.create(HttpStatus.OK, "Cliente removido com sucesso.");
     }
 }
