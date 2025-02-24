b3ccd0653f6ab27d9b305bdb84bf6f9f50314040
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-24 20:41:23 -0300

    feat: add new queries

--- src/main/java/br/com/natfrancisca/euvou/repository/OrganizerRepository.java
+++ src/main/java/br/com/natfrancisca/euvou/repository/OrganizerRepository.java
@@ -3,7 +3,11 @@ package br.com.natfrancisca.euvou.repository;
 import br.com.natfrancisca.euvou.model.Organizer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrganizerRepository extends JpaRepository<Organizer, Long> {
-    boolean findOrganizerBycnpj(String cnpj);
+    Optional<Organizer> findOrganizerByCnpj(String cnpj);
     boolean existsByCnpj(String cnpj);
+    boolean existsByEmail(String email);
+    void deleteByCnpj(String cnpj);
 }
