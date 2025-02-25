56ce6f43168d2638d2dc610abb7dfae5d46f99c3
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-24 23:00:35 -0300

    refactor: refactor the methods for a better code and add new methods

--- src/main/java/br/com/natfrancisca/euvou/controller/TicketsController.java
+++ src/main/java/br/com/natfrancisca/euvou/controller/TicketsController.java
@@ -1,9 +1,12 @@
 package br.com.natfrancisca.euvou.controller;
 
 import br.com.natfrancisca.euvou.dto.APIResponseDTO;
+import br.com.natfrancisca.euvou.dto.TicketsDTO;
 import br.com.natfrancisca.euvou.model.Tickets;
 import br.com.natfrancisca.euvou.service.TicketService;
 import br.com.natfrancisca.euvou.service.TicketsService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,24 +23,36 @@ public class TicketsController {
     }
 
     @PostMapping
-    public ResponseEntity<APIResponseDTO> create(@RequestBody Tickets tickets){
-        Tickets ticketsNew = this.ticketsService.create(tickets);
-        return APIResponseDTO.create(HttpStatus.CREATED, "Informações sobre ingressos cadastrado com sucesso.", ticketsNew);
+    public ResponseEntity<APIResponseDTO> create(@RequestBody @Valid TicketsDTO ticketsDTO){
+        this.ticketsService.create(ticketsDTO.toEntity());
+        return APIResponseDTO.create(HttpStatus.CREATED, "Informações sobre ingressos cadastrado com sucesso.");
     }
 
     @GetMapping
     public ResponseEntity<APIResponseDTO> get(){
-        return APIResponseDTO.create(HttpStatus.OK, this.ticketsService.getAll());
+        return APIResponseDTO.create(HttpStatus.OK, this.ticketsService.get());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<APIResponseDTO> getByID(@PathVariable Long id){
+    public ResponseEntity<APIResponseDTO> getById(@PathVariable Long id){
         return APIResponseDTO.create(HttpStatus.OK, this.ticketsService.getById(id));
     }
 
     @PutMapping("/{id}/manage-access")
-    public ResponseEntity<APIResponseDTO> manageTicketAccess(@PathVariable Long id, @RequestParam boolean access){
-        this.ticketsService.updateTicketAccess(id, access);
-        return APIResponseDTO.create(HttpStatus.CREATED, access ? "Acesso aos ingressos liberado com sucesso." : "Acesso aos ingressos bloqueado com sucesso.");
+    public ResponseEntity<APIResponseDTO> updateAccess(@PathVariable Long id, @RequestParam @Valid boolean access){
+        this.ticketsService.updateAccess(id, access);
+        return APIResponseDTO.create(HttpStatus.OK, access ? "Acesso aos ingressos liberado com sucesso." : "Acesso aos ingressos bloqueado com sucesso.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<APIResponseDTO> update(@PathVariable Long id, @RequestParam @Valid Tickets tickets){
+        return APIResponseDTO.create(HttpStatus.OK, this.ticketsService.update(id, tickets));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<APIResponseDTO> delete(@PathVariable Long id){
+        this.ticketsService.delete(id);
+        return APIResponseDTO.create(HttpStatus.OK, "Informações sobre ingressos deletado com sucesso.");
     }
 }
