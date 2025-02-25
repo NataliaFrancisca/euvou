76404bb0504fd108436ac9b20aadd021b2e09623
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-24 22:58:41 -0300

    feat: delete file

--- src/main/java/br/com/natfrancisca/euvou/exception/TicketsException.java
+++
@@ -1,7 +0,0 @@
-package br.com.natfrancisca.euvou.exception;
-
-public class TicketsException extends IllegalArgumentException{
-    public TicketsException(String message){
-        super(message);
-    }
-}
