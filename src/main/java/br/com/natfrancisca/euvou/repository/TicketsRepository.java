276ae58aba4596ba1c9321d84c48ce9d7dfd112d
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-24 22:59:23 -0300

    feat: add new query

--- src/main/java/br/com/natfrancisca/euvou/repository/TicketsRepository.java
+++ src/main/java/br/com/natfrancisca/euvou/repository/TicketsRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface TicketsRepository extends JpaRepository<Tickets, Long> {
     boolean existsByEventId(Long id);
     Tickets getByEventId(Long id);
+    Optional<Tickets> findByEventId(Long id);
 
     @Query("SELECT COUNT(t) FROM Ticket t WHERE t.event.id = :eventId")
     Long countTicketsByEvent(@Param("eventId") Long eventId);
