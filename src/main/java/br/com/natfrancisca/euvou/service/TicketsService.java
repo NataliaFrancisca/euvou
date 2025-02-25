00f9ca472032b2cf4d95cb1c1b79468164a3380e
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-24 22:59:11 -0300

    refactor: refactor all the methods to avoid duplicated code and create new methods

--- src/main/java/br/com/natfrancisca/euvou/service/TicketsService.java
+++ src/main/java/br/com/natfrancisca/euvou/service/TicketsService.java
@@ -1,14 +1,13 @@
 package br.com.natfrancisca.euvou.service;
 
 import br.com.natfrancisca.euvou.dto.TicketsDTO;
-import br.com.natfrancisca.euvou.exception.EventException;
-import br.com.natfrancisca.euvou.exception.TicketsException;
 import br.com.natfrancisca.euvou.model.Event;
 import br.com.natfrancisca.euvou.model.Tickets;
-import br.com.natfrancisca.euvou.repository.EventRepository;
 import br.com.natfrancisca.euvou.repository.TicketsRepository;
+import br.com.natfrancisca.euvou.util.ValidatorTickets;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,61 +16,67 @@ import java.util.Optional;
 @Service
 public class TicketsService {
     final private TicketsRepository ticketsRepository;
-    final private EventRepository eventRepository;
+    final private ValidatorTickets validatorTickets;
 
     @Autowired
-    public TicketsService(TicketsRepository ticketsRepository, EventRepository eventRepository){
+    public TicketsService(TicketsRepository ticketsRepository, ValidatorTickets validatorTickets){
         this.ticketsRepository = ticketsRepository;
-        this.eventRepository = eventRepository;
+        this.validatorTickets = validatorTickets;
+    }
+
+    private Tickets getTicketsOrCheckTicketsExist(Long id){
+        Optional<Tickets> optionalTickets = this.ticketsRepository.findByEventId(id);
+
+        if(optionalTickets.isEmpty()){
+            throw new EntityNotFoundException("Não há ingressos cadastrados para esse evento.");
+        }
+
+        return optionalTickets.get();
     }
 
     public Tickets create(Tickets tickets){
-        if(this.ticketsRepository.existsByEventId(tickets.getEvent_id())){
-            throw new EventException("Evento já cadastrou acesso aos ingressos.");
-        }
-
-        Optional<Event> event = this.eventRepository.findById(tickets.getEvent_id());
-
-        if(event.isEmpty()){
-            throw new EventException("Não existe evento com esse ID.");
-        }
-
-        if(event.get().getDate().isBefore(tickets.getDateAccessTickets())){
-            throw new TicketsException("Data de acesso aos ingressos deve ser antes da data do evento.");
-        }
-
-        tickets.setEvent(event.get());
+        Event event = this.validatorTickets.validate(tickets);
+        tickets.setEvent(event);
         return this.ticketsRepository.save(tickets);
     }
 
-    public TicketsDTO getById(Long id){
-        Optional<Tickets> ticketsOptional = this.ticketsRepository.findById(id);
-
-        if(ticketsOptional.isEmpty()){
-            throw new EntityNotFoundException("Não há ingressos cadastrados para esse evento.");
-        }
-
-        return TicketsDTO.fromEntity(ticketsOptional.get());
-    }
-
-    public List<TicketsDTO> getAll(){
+    public List<TicketsDTO> get(){
         return this.ticketsRepository.findAll().stream().map(TicketsDTO::fromEntity).toList();
     }
 
-    public void updateTicketAccess(Long id, boolean status){
-        Optional<Tickets> ticketsOptional = this.ticketsRepository.findById(id);
+    public Tickets getById(Long id){
+       return this.getTicketsOrCheckTicketsExist(id);
+    }
 
-        if(ticketsOptional.isEmpty()){
-            throw new EntityNotFoundException("Não há ingressos cadastrados para esse evento.");
+    public Tickets update(Long id, Tickets tickets){
+        Tickets ticketsToUpdate = this.getTicketsOrCheckTicketsExist(id);
+
+        if(!tickets.getEvent_id().equals(ticketsToUpdate.getEvent_id()) && this.ticketsRepository.existsByEventId(tickets.getEvent_id())) {
+            throw new DataIntegrityViolationException("Já existe ingressos para esse evento.");
         }
 
-        Tickets tickets = ticketsOptional.get();
+        ticketsToUpdate.setAmount(tickets.getAmount());
+        ticketsToUpdate.setNumberDaysCloseAccessTickets(tickets.getNumberDaysCloseAccessTickets());
+        ticketsToUpdate.setEvent_id(tickets.getEvent_id());
+        ticketsToUpdate.setDateAccessTickets(tickets.getDateAccessTickets());
+        ticketsToUpdate.setAccessStatus(tickets.isAccessStatus());
+
+        return this.ticketsRepository.save(ticketsToUpdate);
+    }
+
+    public void updateAccess(Long id, boolean status){
+        Tickets tickets = this.getTicketsOrCheckTicketsExist(id);
 
         if(tickets.isAccessStatus() == status){
-            throw new TicketsException("Você está tentando salvar o mesmo dado.");
+            throw new IllegalStateException("Você está tentando salvar o mesmo dado.");
         }
 
         tickets.setAccessStatus(status);
         this.ticketsRepository.save(tickets);
+    }
+
+    public void delete(Long id){
+        Tickets tickets = this.getTicketsOrCheckTicketsExist(id);
+        this.ticketsRepository.deleteById(tickets.getId());
     }
 }
