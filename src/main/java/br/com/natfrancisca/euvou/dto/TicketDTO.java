0786b3d58a533181566421d840084b2ac9d6932b
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-26 21:38:58 -0300

    feat: add validation and create methods toEntity and fromEntity

--- src/main/java/br/com/natfrancisca/euvou/dto/TicketDTO.java
+++ src/main/java/br/com/natfrancisca/euvou/dto/TicketDTO.java
@@ -1,9 +1,7 @@
 package br.com.natfrancisca.euvou.dto;
 
 import br.com.natfrancisca.euvou.model.Client;
-import br.com.natfrancisca.euvou.model.Event;
 import br.com.natfrancisca.euvou.model.Ticket;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,15 +14,35 @@ import lombok.Setter;
 @NoArgsConstructor
 public class TicketDTO {
     private Long id;
+
+    @NotNull(message = "Identificação do evento é obrigatório.")
+    private Long event_id;
+
+    @NotNull(message = "Identificação do cliente é obrigatório.")
+    private Long client_id;
+
     private EventShortDTO event;
     private Client client;
 
-    public static TicketDTO fromEntity(Ticket ticket){
-        return new TicketDTO(
-                ticket.getId(),
-                EventShortDTO.fromEntity(ticket.getEvent()),
-                ticket.getClient()
-        );
+    public Ticket toEntity(){
+        Ticket ticket = new Ticket();
+
+        ticket.setId(this.id);
+        ticket.setEvent_id(this.event_id);
+        ticket.setClient_id(this.client_id);
+
+        return ticket;
     }
 
+    public static TicketDTO fromEntity(Ticket ticket){
+        TicketDTO ticketDTO = new TicketDTO();
+
+        ticketDTO.setId(ticket.getId());
+        ticketDTO.setEvent_id(ticket.getEvent_id());
+        ticketDTO.setClient_id(ticket.getClient_id());
+        ticketDTO.setClient(ticket.getClient());
+        ticketDTO.setEvent(EventShortDTO.fromEntity(ticket.getEvent()));
+
+        return ticketDTO;
+    }
 }
