6ebb43f9a12e989e57522a400de8e7ead154cd3b
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-26 21:39:32 -0300

    feat: add Optional to findByClientCpf

--- src/main/java/br/com/natfrancisca/euvou/repository/TicketRepository.java
+++ src/main/java/br/com/natfrancisca/euvou/repository/TicketRepository.java
@@ -3,7 +3,10 @@ package br.com.natfrancisca.euvou.repository;
 import br.com.natfrancisca.euvou.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
-    boolean existsByClientId(Long idClient);
     boolean existsByEventId(Long id);
+    boolean existsByClientId(Long id);
+    Optional<Ticket> findByClientCpf(String cpf);
 }
